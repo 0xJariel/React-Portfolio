b598d4bda5b6a845dfb8ceb0efcf8636ebf06da8
J <0xjariel@protonmail.com>
2023-10-08 14:32:32 -0600

    proof-reading

--- src/pages/ClayCanvas.jsx
+++ src/pages/ClayCanvas.jsx
@@ -9,7 +9,7 @@ function ClayCanvas() {
       title={"Clay Canvas"}
       img={furnittoImg}
       summary={
-        "A React front end shop with modern routing for selling my future home made pottery collection. The site features a modern shopping cart and dynamic description pages"
+        "A React front-end shop with modern routing for selling my future home made pottery collection. The site features a modern shopping cart and dynamic description pages."
       }
       link={"https://e-commerce-bps1tepqi-0xjariels-projects.vercel.app/"}
       code={"https://github.com/0xJariel/e-commerce"}
